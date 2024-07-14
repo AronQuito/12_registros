2e51d107055a8b862c1dc4ffadd31e3c0a2a8c28
AronQuito <acachicatariq@unjbg.edu.pe>
2024-07-13 22:07:41 -0500

    Actualiza ejercicio 4: mejora y culminado del codigo

--- registros4.cpp
+++ registros4.cpp
@@ -1,4 +1,5 @@
 #include "iostream"
+#include "cstring"
 using namespace std;
 struct atleta{
     char name[30];
@@ -7,21 +8,32 @@ struct atleta{
     int nm;
 };
 int main(){
-    int n;
-    char pais0[10]
-    cout<<"Introducir la cantidad de atletas: ";cin>>n;
+    int n,m=0,k;
+    char pais0[10];
+    cout<<"Introducir la cantidad de atletas: "; cin>>n;
+    int M[n+1];
     atleta lista_atleta[n+1];
     for (int i = 1; i <= n; i++){
-        cout<<"Nombre de aleta "<<i<<": ";cin>>lista_atleta[i].name;
+        cout<<"Nombre de atleta "<<i<<": ";cin>>lista_atleta[i].name;
         cout<<"Pais del atleta "<<i<<": ";cin>>lista_atleta[i].pais;
         cout<<"Disciplina del atleta "<<i<<": ";cin>>lista_atleta[i].dis;
         cout<<"Numero de medallas del atleta "<<i<<": ";cin>>lista_atleta[i].nm;
     }
     cout<<"Introduce un pais: ";cin>>pais0;
     for (int i = 1; i <=n; i++){
-        if (pais0==lista_atleta[i].pais){
+        if (strcmp(pais0, lista_atleta[i].pais) == 0){
             cout<<lista_atleta[i].name<<" viene de este pais."<<endl;
+            m++;
+            M[m]=i;
         } 
     }
+    k=M[1];
+    for (int i = 2; i<=m; i++){
+        if (lista_atleta[k].nm<lista_atleta[M[i]].nm){
+            k=M[i];
+        }
+    }
+    cout<<"El atleta de el pais recogido con mas medallas es: "<<lista_atleta[k].name;
+
     return 0;
 }
